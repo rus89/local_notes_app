4199af494a7c4b81bd7d31da72235d015a94ad88
Milan Rusimov <rusmil89@gmail.com>
2026-01-17 16:56:38 +0100

    Implement MultiProvider for dependency injection and initialize NotesController

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:local_notes_app/controllers/notes_controller.dart';
+import 'package:local_notes_app/data/notes_repository.dart';
 
+//------------------------------------------------------------------------------
 void main() {
-  runApp(const MainApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  final notesRepository = NotesRepository();
+
+  runApp(
+    MultiProvider(
+      providers: [
+        Provider<NotesRepository>.value(value: notesRepository),
+        ChangeNotifierProvider<NotesController>(
+          create: (_) => NotesController(notesRepository),
+        ),
+      ],
+      child: const NoteApp(),
+    ),
+  );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+//------------------------------------------------------------------------------
+class NoteApp extends StatefulWidget {
+  const NoteApp({super.key});
+
+  @override
+  State<NoteApp> createState() => _NoteAppState();
+}
+
+//------------------------------------------------------------------------------
+class _NoteAppState extends State<NoteApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Local Notes',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.greenAccent),
+      ),
+      home: const NotesRoot(),
+    );
+  }
+}
+
+//------------------------------------------------------------------------------
+class NotesRoot extends StatefulWidget {
+  const NotesRoot({super.key});
+
+  @override
+  State<NotesRoot> createState() => _NotesRootState();
+}
+
+//------------------------------------------------------------------------------
+class _NotesRootState extends State<NotesRoot> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<NotesController>().initializeNote();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return Scaffold(
+      appBar: AppBar(title: const Text('Notes')),
+      body: const Center(child: Text('Welcome to Local Notes App!')),
     );
   }
 }
